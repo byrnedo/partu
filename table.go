3748c810b196f4ce1dedbf5b6f45de21a9a12ce5
Donal Byrne <byrnedo@tcd.ie>
2020-07-28 14:57:39 +0200

    Initial commit

---
+++ table.go
@@ -0,0 +1,48 @@
+package partoo
+
+type Table interface {
+	// The name of the ... table
+	TableName() string
+	// The columns mapped to the fields in your struct, by ref please
+	Columns() map[string]interface{}
+}
+
+type ColNames []string
+
+func (c ColNames) Prefix(alias string) ColNames {
+
+	if alias != "" {
+		alias += "."
+	}
+	for i, colName := range c {
+		c[i] = alias + colName
+	}
+	return c
+}
+
+func (c ColNames) Strings() []string {
+	return c
+}
+
+func ColumnNames(t Table) (ret []string) {
+	idx := 0
+	cols := t.Columns()
+	ret = make([]string, len(cols))
+	for k, _ := range cols {
+		ret[idx] = k
+		idx ++
+	}
+	return
+}
+
+func ColumnFields(t Table) (ret []interface{}) {
+
+	idx := 0
+	cols := t.Columns()
+	ret = make([]interface{}, len(cols))
+	for _, v := range cols {
+		ret[idx] = v
+		idx ++
+	}
+	return
+}
